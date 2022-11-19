a00909557c3c46c8a7ed83909bacb46c63bbebe4
Lucas Siqueira <lucassouza.ti@gmail.com>
2022-11-19 15:19:04 -0300

    feat: notification failed imports

---
+++ app/Notifications/ImportNewArticlesFailNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ImportNewArticlesFailNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(public mixed $payload_error)
+    {  }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Houve um erro durante importação de novos artigos.')
+                    ->line('Mensagem: '. $this->payload_error);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- app/Services/Api/ImportNewArticles.php
+++ app/Services/Api/ImportNewArticles.php
@@ -2,21 +2,31 @@
 
 namespace App\Services\Api;
 
+use Exception;
 use App\Models\Article;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\ImportNewArticlesFailNotification;
 
-class ImportNewArticles {
+class ImportNewArticles
+{
 
     public function __invoke()
     {
-        $response = Http::get('https://api.spaceflightnewsapi.net/v3/articles');
+        try {
+            $response = Http::get('https://api.spaceflightnewsapi.net/v3/article');
 
-        if($response->successful())
-        {
-            foreach ($response->json() as $data){
-                Article::updateOrCreate(['id' => $data['id']], $data);
+            if ($response->successful()) {
+                foreach ($response->json() as $data) {
+                    Article::updateOrCreate(['id' => $data['id']], $data);
+                }
+            } else {
+                Notification::route('mail', env('APP_ADMIN_MAIL'))
+                    ->notify(new ImportNewArticlesFailNotification('Erro do servidor - '. $response->body()));
             }
+        } catch (Exception $e) {
+            Notification::route('mail', env('APP_ADMIN_MAIL'))
+                    ->notify(new ImportNewArticlesFailNotification($e->getMessage()));
         }
     }
-
 }
